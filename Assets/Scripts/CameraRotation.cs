30652c3f43701680843dae62be8e92f0c9ffbf03
Sherry <csherry.eddy@gmail.com>
2017-09-30 12:37:06 -0400

    added camera rotation

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start () {
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            RotateCamera();
+        }
+
+        private void RotateCamera()
+        {
+            transform.rotation *= Quaternion.Euler(0, 1, 0); 
+        }
+    }
+}
