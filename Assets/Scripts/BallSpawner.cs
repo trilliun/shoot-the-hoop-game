478bb4d8f4f9bf72d85725fd922e39845de6bc02
Sherry <csherry.eddy@gmail.com>
2017-09-30 11:31:51 -0400

    added ability to create new basketball on spacebar

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BallSpawner : MonoBehaviour
+    {
+        public GameObject BallPrefab;
+
+        // Use this for initialization
+        void Start () {
+		    
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Instantiate(BallPrefab);
+        }
+    }
+}
